f2c07fb8834f4abc194babdae64eaaadadaafb65
Manideep Utla <118208780+ManideepUtla@users.noreply.github.com>
2024-06-30 12:28:04 +0530

    commit to hotflix (#1)

---
+++ Subtraction.java
@@ -0,0 +1,8 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        int a=9;
+        int b=2;
+        int c=a-b;
+        System.out.println("Subtraction of two numbers:" + c);
+    }
+}
