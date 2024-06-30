b60afda30ff99a2cec25addeb991b7db3a193aba
Manideep <manideeputla2002@gmail.com>
2024-06-30 12:21:09 +0530

    commit to main

---
+++ Adding.java
@@ -0,0 +1,9 @@
+public class Adding {
+    public static void main(String[]args){
+        int a=3;
+        int b=5;
+        int c=a+b;
+        System.out.println("adding of two numbers:" + c);
+
+    }
+}
